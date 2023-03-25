d4ebda02b16e0f9b299e47074422c12c13c7b232
Shahin M Hashim <shahinmhashim@gmail.com>
2023-03-25 15:44:48 +0530

    added key terms related to the basics in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+/*
+
+Here are some important terms related to the basics of Flutter:
+
+Widgets: Widgets are the basic building blocks of a Flutter app. Everything that you see on the screen 
+is a widget, from simple text to complex UI elements.
+
+State: In Flutter, state refers to the current condition of a widget. It can change dynamically, and 
+when it does, the widget will be rebuilt.
+
+StatefulWidget: StatefulWidget is a widget that has mutable state. When the state changes, the widget
+is rebuilt.
+
+StatelessWidget: StatelessWidget is a widget that does not have mutable state. Once it is built, it does 
+not change.
+
+MaterialApp: MaterialApp is a predefined widget in Flutter that provides basic app functionality like 
+navigation, theming, and title.
+
+Scaffold: Scaffold is another predefined widget in Flutter that provides a basic visual structure for your 
+app, including a navigation drawer, app bar, and body.
+
+Layouts: In Flutter, layouts are used to position widgets on the screen. There are many types of layouts, 
+such as Row, Column, Stack, and GridView.
+
+ThemeData: ThemeData is a predefined class in Flutter that provides a set of colors and fonts that you can 
+use to style your app.
+
+MediaQuery: MediaQuery is a widget in Flutter that provides information about the device's size and 
+orientation. It can be used to create responsive UIs that adapt to different devices.
+
+Navigator: Navigator is a widget in Flutter that manages the navigation stack. You can use it to navigate 
+between screens and manage the back button.
+
+*/
+
+main() {
+  runApp(app);
+}
